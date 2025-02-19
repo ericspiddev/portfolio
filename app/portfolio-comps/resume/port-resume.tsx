f5a18baec0e1cf6b453966339bbe9e3e64a6046d
Eric Spidle <ericspidledev@gmail.com>
2025-02-18 19:43:03 -0500

    portfolio: add resume component

---
+++ app/portfolio-comps/resume/port-resume.tsx
@@ -0,0 +1,13 @@
+
+export function PortResume() {
+    return (
+        <>
+        <div class="portfolio-header">
+            <p>Resume</p>
+        </div>
+        <div>
+        <p> Here is my resume download <a href="#">here</a> </p> 
+        </div>
+        </>
+    );
+}
