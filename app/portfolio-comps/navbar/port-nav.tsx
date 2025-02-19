cd10987fe5cfb69d3e4fd1c0b30496710662a71c
Eric Spidle <ericspidledev@gmail.com>
2025-02-18 19:42:28 -0500

    portfolio: add navbar component

---
+++ app/portfolio-comps/navbar/port-nav.tsx
@@ -0,0 +1,14 @@
+
+export function PortNavBar()
+{
+    return (
+    <nav id="portfolio-nav-container">
+        <ul id="portfolio-nav">
+            <li class="portfolio-nav-item">Home</li>
+            <li class="portfolio-nav-item">About</li>
+            <li class="portfolio-nav-item">Projects </li>
+            <li class="portfolio-nav-item"><a href="#port-resume">Resume</a></li>
+        </ul>
+    </nav>
+    )
+}
