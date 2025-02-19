cbee30d5d5fa2f2d717d35b49168a56aab5a8052
Eric Spidle <ericspidledev@gmail.com>
2025-02-18 19:42:46 -0500

    portfolio: add about component

---
+++ app/portfolio-comps/about/image-banner.tsx
@@ -0,0 +1,7 @@
+export default function ImageBanner() {
+    return (
+        <div class="photo-container">
+            <img id="eric-spidle"src="images/eric_spidle_portfolio_pic.jpg"  alt="Photo of Eric Spidle"/>
+        </div>
+    );
+}
---
+++ app/portfolio-comps/about/port-about.tsx
@@ -0,0 +1,19 @@
+import ImageBanner from "./image-banner"
+import PortfolioHeader from "../misc/portfolio-header"
+
+export function PortAbout() {
+    const aboutHeaderText = "about";
+    return <>
+        <div id="port-about">
+            <PortfolioHeader header={aboutHeaderText}/> 
+            <ImageBanner/>
+            <div class="about-intro">
+                <h3> Hi. I'm,</h3>
+                <h1>Eric Spidle</h1>
+            </div>
+            <div class="about-section">
+            <p>An embedded systems/web developer that has a love for technology. My development journey began when I was 10 years old and trying to figure out how I could mod Minecraft. Since then I've enjoyed learning everything there is about software and how it works. My areas of specialty are embedded systems, specifically hypervisors and embedded Linux, and web development where I have experience with both ASP MVC and Ruby on Rails. I love solving complex problems and building out solutions that improve a company or person's everyday.</p>
+            </div>
+        </div>
+    </>
+}
