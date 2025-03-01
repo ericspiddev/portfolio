46046849a451f926fd10c4152218be572142edbe
Eric Spidle <ericspidledev@gmail.com>
2025-02-28 21:25:15 -0500

    about: add code background and format

--- app/portfolio-comps/about/image-banner.tsx
+++ app/portfolio-comps/about/image-banner.tsx
@@ -1,7 +1,19 @@
+import { useState} from 'react';
+
 export default function ImageBanner() {
+    const name = "<Eric Spidle/>";
+    let [count, setCount] = useState(0);
+
     return (
+    <>
         <div class="photo-container">
+            <div id="code-background"></div>
             <img id="eric-spidle"src="images/eric_spidle_portfolio_pic.jpg"  alt="Photo of Eric Spidle"/>
+            <div class="about-intro">
+                <h3> Hi. I'm,</h3>
+                <h1>{name}</h1>
+            </div>
         </div>
+    </>
     );
 }
