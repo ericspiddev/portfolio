3edcd0686b2bf66fa0a414207868c5f7299a0202
Eric Spidle <ericspidledev@gmail.com>
2025-02-28 21:41:52 -0500

    blog: add blog section

---
+++ app/portfolio-comps/blog/port-blog.tsx
@@ -0,0 +1,13 @@
+import PortfolioHeader from "../misc/portfolio-header"
+export function PortBlog() { 
+    return (
+        <>
+            <div class="section-common">
+                <div class ="section-text-common"> 
+                <PortfolioHeader header="Blog"/>
+                <p> Blog if we have one here </p> 
+                </div>
+            </div>
+        </>
+    );
+}
