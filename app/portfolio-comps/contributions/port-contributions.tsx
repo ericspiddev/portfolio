b66edc0f901b9f9a5319220a72047be565699d6b
Eric Spidle <ericspidledev@gmail.com>
2025-02-28 21:54:57 -0500

    contributions: add contributions section

---
+++ app/portfolio-comps/contributions/port-contributions.tsx
@@ -0,0 +1,13 @@
+import PortfolioHeader from "../misc/portfolio-header"
+export function PortContributions() {
+    return (
+        <>
+            <div class="section-common">
+                <div class="section-text-common">
+                    <PortfolioHeader header="Contributions"/>
+                    <p> Open source contributions here </p>
+                </div>
+            </div>
+        </>
+    );
+}
