21c5d1e68ee129c440184060849bb24d3ef3a81c
Eric Spidle <ericspidledev@gmail.com>
2025-02-28 21:10:29 -0500

    navbar: add icons and sections

--- app/portfolio-comps/navbar/port-nav.tsx
+++ app/portfolio-comps/navbar/port-nav.tsx
@@ -1,14 +1,21 @@
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faCircleInfo, faCode, faCodeCommit, faComment, faEnvelope} from "@fortawesome/free-solid-svg-icons";
 export function PortNavBar()
 {
     return (
     <nav id="portfolio-nav-container">
         <ul id="portfolio-nav">
-            <li class="portfolio-nav-item">Home</li>
-            <li class="portfolio-nav-item">About</li>
-            <li class="portfolio-nav-item">Projects </li>
-            <li class="portfolio-nav-item"><a href="#port-resume">Resume</a></li>
-        </ul>
+            <li class="portfolio-nav-item"><a href="#about-section"> <FontAwesomeIcon icon={faCircleInfo}/> About</a> </li>
+            <li class="portfolio-nav-item"><a href="#portfolio-section"> <FontAwesomeIcon icon={faCode}/> Portfolio </a></li>
+            <li class="portfolio-nav-item"> <a href="#contributions-section"> <FontAwesomeIcon icon={faCodeCommit}/> Contributions</a></li>
+            <li class="portfolio-nav-item"> <a href="#blog-section"> <FontAwesomeIcon icon={faComment}/> Blog</a></li>
+            <li class="portfolio-nav-item"><a href="#contact-section"> <FontAwesomeIcon icon={faEnvelope}/> Contact</a></li>
+            <li></li>
+            <div id="portfolio-nav-banner">
+                <p>Eric Spidle</p>
+            </div>
+        </ul> 
     </nav>
     )
 }
