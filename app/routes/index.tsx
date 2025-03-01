063412bbcd6c77a46126297c650d7e698dc8ad3b
Eric Spidle <ericspidledev@gmail.com>
2025-02-28 21:55:44 -0500

    index: add section comps and fontawesome kit?

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,9 @@
 import { PortNavBar } from "../portfolio-comps/navbar/port-nav";
 import { PortAbout } from "../portfolio-comps/about/port-about";
-import { PortResume } from "../portfolio-comps/resume/port-resume";
+import { PortPortfolio } from "../portfolio-comps/portfolio/port-portfolio";
+import { PortResume } from "../portfolio-comps/contact/port-resume";
+import { PortContributions } from "../portfolio-comps/contributions/port-contributions";
+import { PortBlog } from "../portfolio-comps/blog/port-blog";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,9 +14,13 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
     return (
         <>
+            <script src="https://kit.fontawesome.com/b24df79285.js" crossorigin="anonymous"></script>
             <PortNavBar/>
             <PortAbout/>
+            <PortPortfolio/>
             <PortResume/>
+            <PortContributions/>
+            <PortBlog/>
         </>
     );
 }
