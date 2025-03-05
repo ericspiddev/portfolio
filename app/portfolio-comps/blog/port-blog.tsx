b89b949be05ac000d4ba11fb6c36a4d95d5ab8e6
Eric Spidle <ericspidledev@gmail.com>
2025-03-04 23:00:21 -0500

    blog: implement card like links (still need to get sizing down)

--- app/portfolio-comps/blog/port-blog.tsx
+++ app/portfolio-comps/blog/port-blog.tsx
@@ -9,21 +9,45 @@ export interface BlogObject
     desc : string
 };
 
-const test: BlogObject = {
-    link: "https://www.dornerworks.com/blog/sel4-on-risc-v-rocket-chip/",
-    title: "seL4 Virtualized on Risc-V",
-    previewImage: "public/images/blog/riscv-blog.png",
-    desc: "Article about getting an seL4 VM running on the RISC-V architechture"
-};
+const blogs: BlogObject[] = [
+    {
+        link: "https://www.dornerworks.com/blog/sel4-on-risc-v-rocket-chip/",
+        title: "seL4 Virtualized on Risc-V",
+        previewImage: "public/images/blog/riscv-blog.png",
+        desc: "Article about getting an seL4 VM running on the RISC-V architechture" 
+    },
+    {
+        link: "#",
+        title: "Coming soon",
+        previewImage: "public/images/blog/placeholder.jpg",
+        desc: "A new article on an interesting topic is on it's way" 
+        
+    },
+    {
+        link: "#",
+        title: "Coming soon",
+        previewImage: "public/images/blog/placeholder.jpg",
+        desc: "A new article on an interesting topic is on it's way" 
+    },
+];
+
 
 export function PortBlog() { 
     return (
         <>
             <div class="section-common">
                 <div class ="section-text-common"> 
-                <PortfolioHeader header="Blog"/>
-                <p> Articles I've written about all sorts of topics</p> 
-                <PortBlogEntry blog={test}/>
+                    <PortfolioHeader header="Blog"/>
+                    <p> Articles I've written about all sorts of topics</p> 
+                    <div id="blogs-container">
+                        {
+                            blogs.map((blog) => (
+                            <PortBlogEntry blog={blog}/>))
+                        }
+                    </div>
+                    <div class="filler">
+                        <p> MORE &gt;  </p>
+                    </div>
                 </div>
             </div>
         </>
