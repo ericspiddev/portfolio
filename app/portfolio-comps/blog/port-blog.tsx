98cb645180339a97fe6ffea592bd26b078a3778e
Eric Spidle <ericspidledev@gmail.com>
2025-03-05 22:18:03 -0500

    tmp/blog: add id field to blog objects to  make react happy

--- app/portfolio-comps/blog/port-blog.tsx
+++ app/portfolio-comps/blog/port-blog.tsx
@@ -11,35 +11,42 @@ export interface BlogObject
 
 const blogs: BlogObject[] = [
     {
+        id: "1",
         link: "https://www.dornerworks.com/blog/sel4-on-risc-v-rocket-chip/",
         title: "seL4 Virtualized on Risc-V",
         previewImage: "public/images/blog/riscv-blog.png",
         desc: "Article about getting an seL4 VM running on the RISC-V architechture" 
     },
-    {   link: "#",
+    {   
+        id: "2",
+        link: "#",
         title: "Coming soon",
         previewImage: "public/images/blog/placeholder.jpg", 
         desc: "A new article on an interesting topic is on it's way"  
     },
     {
+        id: "3",
         link: "#",
         title: "Coming soon",
         previewImage: "public/images/blog/placeholder.jpg",
         desc: "A new article on an interesting topic is on it's way" 
     },
     {
+        id: "4",
         link: "#",
         title: "Coming soon",
         previewImage: "public/images/blog/placeholder.jpg",
         desc: "A new article on an interesting topic is on it's way" 
     },
     {
+        id: "5",
         link: "#",
         title: "Coming soon",
         previewImage: "public/images/blog/placeholder.jpg",
         desc: "A new article on an interesting topic is on it's way" 
     },
     {
+        id: "6",
         link: "#",
         title: "Coming soon",
         previewImage: "public/images/blog/placeholder.jpg",
