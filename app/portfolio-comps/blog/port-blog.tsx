82c47f4e481d172599c0c30e9fb75a1e36b63d1a
Eric Spidle <ericspidledev@gmail.com>
2025-03-09 16:20:43 -0400

    blogs/trivial: remove public from images names

--- app/portfolio-comps/blog/port-blog.tsx
+++ app/portfolio-comps/blog/port-blog.tsx
@@ -14,42 +14,42 @@ const blogs: BlogObject[] = [
         id: "1",
         link: "https://www.dornerworks.com/blog/sel4-on-risc-v-rocket-chip/",
         title: "seL4 Virtualized on Risc-V",
-        previewImage: "public/images/blog/riscv-blog.png",
+        previewImage: "/images/blog/riscv-blog.png",
         desc: "Article about getting an seL4 VM running on the RISC-V architechture" 
     },
     {   
         id: "2",
         link: "#",
         title: "Coming soon",
-        previewImage: "public/images/blog/placeholder.jpg", 
+        previewImage: "/images/blog/placeholder.jpg", 
         desc: "A new article on an interesting topic is on it's way"  
     },
     {
         id: "3",
         link: "#",
         title: "Coming soon",
-        previewImage: "public/images/blog/placeholder.jpg",
+        previewImage: "/images/blog/placeholder.jpg",
         desc: "A new article on an interesting topic is on it's way" 
     },
     {
         id: "4",
         link: "#",
         title: "Coming soon",
-        previewImage: "public/images/blog/placeholder.jpg",
+        previewImage: "/images/blog/placeholder.jpg",
         desc: "A new article on an interesting topic is on it's way" 
     },
     {
         id: "5",
         link: "#",
         title: "Coming soon",
-        previewImage: "public/images/blog/placeholder.jpg",
+        previewImage: "/images/blog/placeholder.jpg",
         desc: "A new article on an interesting topic is on it's way" 
     },
     {
         id: "6",
         link: "#",
         title: "Coming soon",
-        previewImage: "public/images/blog/placeholder.jpg",
+        previewImage: "/images/blog/placeholder.jpg",
         desc: "A new article on an interesting topic is on it's way" 
     },
 ];
