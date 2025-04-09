7db248a4d68e46e581ec725c24d15e09e0b663f2
Eric Spidle <ericspidledev@gmail.com>
2025-04-09 18:25:27 -0400

    Navbar/Hamburger: now close on outside clicks and some fixups

--- app/portfolio-comps/mobile/hamburger-menu/hamburger-menu.tsx
+++ app/portfolio-comps/mobile/hamburger-menu/hamburger-menu.tsx
@@ -1,28 +1,45 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {useState} from 'react';
+import {useState, useRef, useEffect} from 'react';
 import {faBars, faCircleInfo, faCode, faCodeCommit, faComment, faEnvelope} from "@fortawesome/free-solid-svg-icons";
 
 
 export function HamburgerMenu() {
     let [show, setShow] = useState(false);
+    const menuRef = useRef(null);
+    const buttonRef = useRef(null);
 
     const handleHamburgerClick = () => {
         console.log(show);
         setShow(!show);
     }
+
+    useEffect(() => {
+        function closeOnOutsideClick(event) {
+            if (menuRef.current && !menuRef.current.contains(event.target)
+            && buttonRef.current && !buttonRef.current.contains(event.target)) {
+                setShow(false);
+            }
+        }
+
+        document.addEventListener("mousedown", closeOnOutsideClick);
+        return () => {
+            document.removeEventListener("mousedown", closeOnOutsideClick);
+        };
+    }, []);
+
     return (
     <>
         <div class="hamburger-toggle">
-        <FontAwesomeIcon icon={faBars} onClick={handleHamburgerClick}/>
+            <FontAwesomeIcon icon={faBars} onClick={handleHamburgerClick} ref={buttonRef} size="lg"/>
         </div>
 
-        <div class= {show ? 'hamburger-menu' : 'hidden'} >
+        <div class= {show ? 'hamburger-menu' : 'hidden'} ref={menuRef}>
         <ul>
-            <li ><a href="#about-section"> <FontAwesomeIcon icon={faCircleInfo}/> About</a> </li>
-            <li ><a href="#portfolio-section"> <FontAwesomeIcon icon={faCode}/> Portfolio </a></li>
-            <li ><a href="#contact-section"> <FontAwesomeIcon icon={faEnvelope}/> Contact</a></li>
-            <li > <a href="#blog-section"> <FontAwesomeIcon icon={faComment}/> Blog</a></li>
-            <li > <a href="#contributions-section"> <FontAwesomeIcon icon={faCodeCommit}/> Contributions</a></li>
+            <li onClick={handleHamburgerClick} ><a href="#about-section"> <FontAwesomeIcon icon={faCircleInfo}/> About</a> </li>
+            <li onClick={handleHamburgerClick}><a href="#portfolio-section"> <FontAwesomeIcon icon={faCode}/> Portfolio </a></li>
+            <li onClick={handleHamburgerClick}><a href="#contact-section"> <FontAwesomeIcon icon={faEnvelope}/> Contact</a></li>
+            <li onClick={handleHamburgerClick}> <a href="#blog-section"> <FontAwesomeIcon icon={faComment}/> Blog</a></li>
+            <li onClick={handleHamburgerClick}> <a href="#contributions-section"> <FontAwesomeIcon icon={faCodeCommit}/> Contributions</a></li>
         </ul>
         </div>
     </>
