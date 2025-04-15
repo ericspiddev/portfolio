56a2a3af28a7a798db8df50f5a7d6ca541d598d4
Eric Spidle <ericspidledev@gmail.com>
2025-04-15 18:42:25 -0400

    fullscreen: use fullscreen modals on smaller screens

--- app/portfolio-comps/portfolio/port-project-modal.tsx
+++ app/portfolio-comps/portfolio/port-project-modal.tsx
@@ -1,6 +1,7 @@
 import {useState} from "react";
 import {ProjModalTabs} from "./project-modal-tab";
 import {ProjModalTabContent} from "./project-modal-tab-content";
+import {PortProjectResult} from "./project-result";
 
 export function PortProjectModal({project, closeModal, showModal}){
 
@@ -47,6 +48,14 @@ export function PortProjectModal({project, closeModal, showModal}){
                         <ProjModalTabContent tabs={tabs} currTab={tab}/>
                     </div>
                     <img class= {`modal-preview-img ${project.previewImg == '' ? 'hidden' : '' }`} src={`images/projects/modal/${project.previewImg}`}/>
+                </div>
+                <div class="modal-content-fullscreen">
+                    <p className="modal-project-title"> {project.title}</p>
+                    <img className= {`modal-preview-img ${project.previewImg == '' ? 'hidden' : '' }`} src={`images/projects/modal/${project.previewImg}`}/>
+                    <p className="modal-section-header"> {tabs[0].title}</p>
+                    <p className={`modal-project-desc`}> {tabs[0].content}</p>
+                    <p className="modal-section-header"> {tabs[1].title}</p>
+                    <PortProjectResult results={tabs[1].content}/>
                 </div>
             </div>
         </div>
