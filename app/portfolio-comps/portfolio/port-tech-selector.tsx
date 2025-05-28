ff3199014b48908e59330f97a3792ee40c7141ce
Eric Spidle <ericspidledev@gmail.com>
2025-05-27 23:06:02 -0400

    projects/tech: highlight logos for 5 seconds if user clicks on category

--- app/portfolio-comps/portfolio/port-tech-selector.tsx
+++ app/portfolio-comps/portfolio/port-tech-selector.tsx
@@ -1,4 +1,4 @@
-
+import {useEffect} from "react";
 
 function handleTechMouseEnter(setState) {
     setState(true);
@@ -7,11 +7,21 @@ function handleTechMouseLeave(setState) {
     setState(false);
 }
 
+const highlightTimeSecs = 5;
+
+
 export function PortTechSelector({state, setState, selectorText}) {
+    useEffect(() => {
+      const highlightTime = setInterval(() => {
+        setState(false);
+      }, highlightTimeSecs * 1000); // let banner linger for 3s before clearing
+        return () => clearInterval(highlightTime);
+    }, [state]);
     return(
         <button
             onMouseEnter={() => handleTechMouseEnter(setState)}
             onMouseLeave={() => handleTechMouseLeave(setState)}
+            onClick={() => setState(true)}
             class="tech-selector">{selectorText}</button>
     );
 }
