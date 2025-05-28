e7d96ccd4b0d8817f794c6bcb4995c36393ec8bc
Eric Spidle <ericspidledev@gmail.com>
2025-05-27 23:04:27 -0400

    projects: finish up project modals and cleanup some descriptions

--- app/portfolio-comps/portfolio/port-project-data.tsx
+++ app/portfolio-comps/portfolio/port-project-data.tsx
@@ -15,9 +15,9 @@ export interface ModalProject
 const lunarLoggerDesc = `
 Lunar logger is a tracking app used for managing the status and tasks of projects.
 Written using dotnet's ASP MVC it allows users to create issues within projects to track the progress
-of tasks. The web app allows users to add other users to a proejct and assign issues to them.
+of tasks. The web app allows users to add other users to a project and assign issues to them.
 Each project consists of categories and each category holds issues. Switching issues between
-categories is as simple as click and dragging the issue from one column to the other.
+categories is as simple as drag and dropping the issue from one column to the other.
 The issues all track the created date, modified date, and estimated time for completion.
 `
 const lunarLoggerResults = [
@@ -28,10 +28,10 @@ const lunarLoggerResults = [
 ]
 
 const networkedCoffeeDesc = `
-Ever wanted to make coffee from the comfort of your own bed? That is certianly possibly these days
+Ever wanted to make coffee from the comfort of your own bed? That is certainly possibly these days
 with smart coffee makers allowing you access from your phone. This project I created my own smart coffee
 maker using an ESP32 and wifi connectivity. The application allows you to send an HTTP message over you wifi
-network to toggle a coffee machine on/off. I plan am on creating a full functional website for the
+network to toggle a coffee machine on/off. I plan on creating a full functional website for the
 coffee maker that can be used to control it in a user friendly way. I also want to create a PCB for the necessary
 circuitry to clean things up. (The initial project was heavily inspired by a video done by Low Level Learning on Youtube)
 This project utilized embedded C and the ESP-IDF. It also required a good understanding of the HTTP protocol and some web
@@ -47,7 +47,7 @@ const networkedCoffeeResults = [
 
 const magicMirrorDesc = `
 The magic mirror is a project I did where using two way glass, a raspberry pi and the
-magic mirror repo (need to add link). I created a smart mirror that displayed all sorts of
+magic mirror repo. I created a smart mirror that displayed all sorts of
 different things. The date, time, custom messages, current news headlines. Most of these come
 with the base magic mirror repo but I am in the process of creating some of my own MM modules.
 The first one I plan to make is a module that shows/generates a daily workout plan each day.
@@ -56,7 +56,7 @@ and configring magic mirror. Also some real world hardware skills for connecting
 and actually building the mirror and frame.
 `
 const magicMirrorResults = [
-    "Setup a rapsberry pi and rapsdebian for running the mirror",
+    "Setup a rapsberry pi and raspdebian for running the mirror",
     "Built a frame and backing structure to display the laptop screen through 2-way glass",
     "Customized some modules to include messages that were relevant to me",
     "Setup static IP/network configs and SSH to allow for easy debug and config access",
@@ -73,7 +73,10 @@ at GVSU's project showcase (voted on by classmates). It utilized embedded C and 
 systems like I2C, SPI, as well as other lowlevel concepts
 `
 const dashboardResults = [
-    "Used common embedded protocols to archi",
+    "Created a dashboard that reported speed, miles travelled, RPM, and turn signals",
+    "Used I2C and SPI to communicate with multiple peripherals including EPROM to save settings between power cycles",
+    "Added interactive LCD for users to set and get various settings on the dashboard",
+    "Supported communuication via bluetooth and a phone using a COTS bluetooth chip and the MSP432",
     "Voted 1 of 3 projects to attend Grand Valley's Project Showcase for best in class",
 
 ];
@@ -95,7 +98,7 @@ const napsterResults =[
 
 const pcbDesc = `
 This project was for a circuit class I took in my undergrad where we were tasked with creating a speaker using a PCB board.
-The design allowed for output of two speakers, and a microphone that amplified a users voice. It also took in a aux cord in
+The design allowed for output of two speakers, and a microphone that amplified a users voice. It also took in an aux cord in
 order to play sound so music from a phone could be played over the speaker. The speaker was also required to be adjustable in volume
 using potentiometers. This project was also selected to be 1/3 shown off at GVSU's project showcase (voted on by classmates). This project involved a
 great understanding of circuit design and the differnt types of circuits that go into turning sound waves to amplified speaker output.
@@ -131,7 +134,10 @@ the Dexer Pay API to allow for a better mobile experience. I also used AWS Cloud
 laundromats all over the world
 `
 const dexterLiveResults = [
-
+    "Made improvements to the Ruby on Rails Dexter Live app and all of its associated applications",
+    "Integrated Cloudfront with DexterLive so users all over the world could dowload firmware images with better speeds",
+    "Updated DexterLive's Pay API to include new models of Washers/Dryers as well as the capacity and error status of each",
+    "Revamped the DexterLive internet speed test to use simulated firmware images to give owners a better idea of their real internet speeds",
 ];
 
 const dexterControlsDesc = `
@@ -141,6 +147,12 @@ exposure to different tech. Sometimes I would be adding recipes or packages to t
 or improvements in the C++ application that handled money and user input. I also taught myself QML/Qt to fulfill Dexter's needs when
 our main GUI developer left Dornerworks.`
 const dexterControlsResults = [
+    "Supported and improved Dexter's Dryer/Washer firmware stack",
+    "Created both features and bug fixes for the full stack of Software running on Dexter Washers/Dryers",
+    "Added a help menu for users to give instructions on how to load laundry and start cycles",
+    "Improved the integrity of card payments by adding CRC16 field and checks in the touchscreen application",
+    "Reworked the entire Dryer cycle logic in both the firmware and touchscreen application to fix bugs and improve future maintainability",
+    "Fixed numerous bugs that spanned across Yocto, U-Boot and the touchscreen application."
 
 ];
 
@@ -154,7 +166,10 @@ working in seL4 but I also created a few USB device drivers. This proved out the
 moved to other projects but my work was the starting point for adding USB Hub support and cleaning up the driver code.
 `
 const xhciUsbResults = [
-
+    "Ported the XHCI U-boot driver over to seL4's components",
+    "Debugged and fixed various USB drivers already present in seL4 that didn't work with XHCI",
+    "Wrote drivers for several classes of USB drivers including HID and CDC",
+    "Started implementation for hub support in XHCI devices before the project's phase ended"
 ];
 
 const droneDemoDesc = `
@@ -162,11 +177,15 @@ One of my first tasks as a full time engineer at Dornerworks was to overhaul an 
 interactive for potential customers. My work for this demo involved getting Airsim and Ardupilot talking to each other
 via seL4 VMs on new ruggadized hardware. I first had to port the existing demo to the ruggadized hardware in seL4 before I could
 start work on any of the improvements. This involved working with Yocto recipes and DTS files to get everything in order. Once
-this was done I then used Ardupilot's android app and Airsim on a laptop to make my imrpovements. Using routing rules in Linux and some
+this was done I then used Ardupilot's android app and Airsim on a laptop to make my improvements. Using routing rules in Linux and some
 configuration I was able to have the Ardupilot app send drone commands through seL4's VM and then display the result in Airsim.
 `
 const droneDemoResults= [
-
+    "Reworked an existing demo on COTS hardware to show off containers in seL4 using AirSim, Ardupilot, and Mission Planner",
+    "Made the necessary changes to boot the original demo on the NAI68ARM2 hardware",
+    "Fixed an issue with the old demo where it was using localhost in Airsim rather then actual ethernet traffic",
+    "Integrated a tablet and the mission planner app to make the demo interactive",
+    "Setup Linux ip routing rules to allow mission planner to communicate with the docker container running in a VM",
 ]
 const hwMgrDesc = `
 hw-mgr was a tool I developed for Dornerworks (DW) as an intern. During Covid-19 many people were forced to work remotely and this caused
@@ -177,13 +196,17 @@ well. This not only led to increased productivty but also protected more expensi
 gitlab's API and bash to create a remote testing enviornment that engineers all across DW could use.
 `
 const hwMgrResults = [
-
+    "Created a hardware checkout system so remote workers could easily share hardware for testing",
+    "Protected hardware by keeping it in one place rather then constantly moving between people",
+    "Improved efficiency for new members to get up and running with testing hardware",
+    "Used Gitlab's API to block checkout for any device being used by a CI/CD pipeline.",
+    "Made it easy for anyone to add or remove hardware and lock down hardware to specific projects using Gitlab's API"
 ]
 
 const rcCarDesc =`
-Using a ESP32 and Android studio I and a team of engineering students were able to write a app that connected to the
+Using a ESP32 and Android studio I and a team of engineering students were able to write an app that connected to the
 ESP32 over bluetooth and drove a remote control car around. My part in this was designing and getting the bluetooth
-Android app working. I had never done any mobile development before this project and the class was only 6 weeks long.
+Android app working. The others focused on the RC car. I had never done any mobile development before this project and the class was only 6 weeks long.
 Using Android studio I was able to get the application hooked up over bluetooth and sending data. I also designed a very
 simple control interface to send the direction and speed at which the car should drive. I am proud of this project
 since I was able to learn a new technology in such a short amount of time.
@@ -195,33 +218,36 @@ const rcCarResults = [
     "Completed the course in about 6 Weeks without ever writing a mobile app before",
 ]
 const riscvSel4GuestDesc = `
-Risc-V is an up and coming architecture that allows chip designers to use an open and royalty free ISA set to bring their
+Risc-V is an up and coming architecture that allows chip designers to use an open and royalty free ISA to bring their
 boards to market. While I was an intern at DW my task across 2 rotations was to get a guest running under seL4 on a RISC-V
 softcore Rocketchip. The rocketchip ran on the PL of a ZYNQMP SoC. I completed this work but not without some help
 (Thank you Robert Vanvossen and Michael Doran). At the time this was considered to be the first RISC-V guest running on seL4
-on real hardware (QEMU was done before this). I wrote an article about which can be found below in the blogs section.
+on real hardware (QEMU was done before this). I wrote an article about this which can be found below in the blogs section.
 `
 const riscvSel4GuestResults = [
-
+    "Successfully booted a buildroot linux guest underneath seL4 on RISC-V.",
+    "Debugged various issues when trying to boot the guest that resulted in digging through RISC-V assembly",
+    "Added support to the seL4 RISC-V VMM for 16 bit instructions (similar to Thumb2 on ARM)",
+    "Fixed some issues with interrupts and ACKing in the RISC-V VMM with some help (Thanks Robbie)",
+    "Received an outstanding techncial contribution award at DW as a co-op for this task",
 ];
 
 export const projects : ModalProject[] = [
-    createProject("Lunar Logger", lunarLoggerDesc, "rocket.svg", false, 0, "lunar-logger-demo.png", lunarLoggerResults, "videos/test-video.mp4"),
+    createProject("Lunar Logger", lunarLoggerDesc, "rocket.svg", false, 0, "lunar-logger-demo.png", lunarLoggerResults, "lunar-logger.mp4"),
     createProject("Networked Coffee", networkedCoffeeDesc, "coffee.svg", false, 0, "coffee-maker.svg", networkedCoffeeResults),
     createProject("Car Dashboard", dashboardDesc, "speedometer.svg", false, 2, "dashboard-demo.jpg", dashboardResults),
-    createProject("seL4 Doom Pendulum",doomPendDesc, "video-games.svg", false, 2, "doom-pend-demo.jpg", doomPendResults),
+    createProject("seL4 Doom Pendulum",doomPendDesc, "video-games.svg", false, 2, "doom-pend-demo.jpg", doomPendResults, "doom-demo.mp4"),
     createProject("Magic Mirror", magicMirrorDesc, "mirror.svg",false, 0, "magic-mirror.png", magicMirrorResults),
     createProject("Dexter Live", dexterLiveDesc, "wifi.svg", false, 1, 'dexter-live.png', dexterLiveResults),
-    createProject("Dexter Controls", dexterControlsDesc, "laundry.svg", false, 1, '', dexterControlsResults),
+    createProject("Dexter Controls", dexterControlsDesc, "laundry.svg", false, 1, 'dexter-controls.png', dexterControlsResults),
     createProject("seL4 XHCI USB", xhciUsbDesc, "usb.svg", false, 1, 'usb.jpg', xhciUsbResults),
     createProject("Hw-Mgr", hwMgrDesc, "share.svg", false, 1, 'hw-mgr.png', hwMgrResults),
     createProject("seL4 Drone Demo", droneDemoDesc, "drone.svg", false, 1, 'ardupilot-planner.jpg', droneDemoResults),
     createProject("RISC-V seL4 Guest", riscvSel4GuestDesc, "riscv.svg", false, 1, 'risc-v.png', riscvSel4GuestResults),
     createProject("GV Napster", napsterDesc, "data-transfer.svg", false, 2, "gv-napster-demo.png", napsterResults),
     createProject("Stereo PCB", pcbDesc, "music.svg", false, 2, "pcb.jpg", pcbResults ),
-    createProject("Bluetooth RC Car", rcCarDesc, "rc.svg", false, 2, '', rcCarResults),
+    createProject("Bluetooth RC Car", rcCarDesc, "rc.svg", false, 2, "android-rc-app.png", rcCarResults, "rc-car.mp4"),
 ];
-
 
 function createProject(title, desc, projectImg, hasGallery=false, projectType, previewImg, results, demo) : ModalProject
 {
