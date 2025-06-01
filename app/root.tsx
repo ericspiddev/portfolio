13b6a2e1aba953f5b347789e4b8623e35bf269bc
Eric Spidle <ericspidledev@gmail.com>
2025-06-01 15:18:03 -0400

    app: add laptop favicon

--- app/root.tsx
+++ app/root.tsx
@@ -35,6 +35,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png"/>
+        <link rel="manifest" href="/images/favicon/site.webmanifest"/>
         <Links />
       </head>
       <body>
