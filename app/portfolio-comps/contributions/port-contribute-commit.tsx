34c8d93fb669261fbad64e5de76b3a8a2c68ae21
Eric Spidle <ericspidledev@gmail.com>
2025-06-01 13:02:46 -0400

    trivial: remove public from commit SHA image

--- app/portfolio-comps/contributions/port-contribute-commit.tsx
+++ app/portfolio-comps/contributions/port-contribute-commit.tsx
@@ -4,7 +4,7 @@ export function PortContributeCommit({ commit }) {
     return (
     <>
         <div className="commit">
-            <div className="commit-sha"><img className="commit-img" src="public/images/contribute/commit.svg"/> {commit.sha.slice(0, 8)} </div>
+            <div className="commit-sha"><img className="commit-img" src="images/contribute/commit.svg"/> {commit.sha.slice(0, 8)} </div>
             <p className="commit-message"> "{commit.message}" </p>
             <p className="commit-author"> @{commit.author}</p>
         </div>
