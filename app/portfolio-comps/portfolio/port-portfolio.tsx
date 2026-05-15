6547e0c059a94eb9a33a1b764241eb3c5d84f5d8
Eric Spidle <ericspidledev@gmail.com>
2026-05-15 14:10:06 -0400

    tabs: make portfolio tabs clickable

--- app/portfolio-comps/portfolio/port-portfolio.tsx
+++ app/portfolio-comps/portfolio/port-portfolio.tsx
@@ -16,17 +16,17 @@ export function PortPortfolio() {
                     </div>
                     <div className="portfolio-tabs section-text-common">
                         <div className="portfolio-tab-header">
-                        <button className="portfolio-tab"
-                                id={tab == portfolioTabTypes.PROFESSIONAL ? 'active-tab': ''}
-                                onClick = {() => setTab(portfolioTabTypes.PROFESSIONAL)}>
-                        Professional
-                        </button>
-                        <button className="portfolio-tab"
+                        <button className="clickable portfolio-tab"
                                 id={tab == portfolioTabTypes.PERSONAL ? 'active-tab': ''}
                                 onClick = {() => setTab(portfolioTabTypes.PERSONAL)}>
                         Personal
                         </button>
-                        <button className="portfolio-tab"
+                        <button className="clickable portfolio-tab"
+                                id={tab == portfolioTabTypes.PROFESSIONAL ? 'active-tab': ''}
+                                onClick = {() => setTab(portfolioTabTypes.PROFESSIONAL)}>
+                        Professional
+                        </button>
+                        <button className="clickable portfolio-tab"
                                 id={tab == portfolioTabTypes.EDUCATIONAL ? 'active-tab': ''}
                                 onClick = {() => setTab(portfolioTabTypes.EDUCATIONAL)}>
                         Educational
