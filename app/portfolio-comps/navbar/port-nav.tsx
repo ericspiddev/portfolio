e05f6b15ff66762844be16fc79f80e82bcd17786
Eric Spidle <ericspidledev@gmail.com>
2026-05-21 22:25:42 -0400

    nav: add gap to nav items and remove underlines

--- app/portfolio-comps/navbar/port-nav.tsx
+++ app/portfolio-comps/navbar/port-nav.tsx
@@ -12,11 +12,11 @@ export function PortNavBar()
                 </a>
             </div>
         <ul id="portfolio-nav">
-            <li className="hover-enlarge portfolio-nav-item"><a href="#about-section"> <FontAwesomeIcon icon={faCircleInfo}/> About</a> </li>
-            <li className="hover-enlarge portfolio-nav-item"><a href="#portfolio-section"> <FontAwesomeIcon icon={faCode}/> Portfolio </a></li>
-            <li className="hover-enlarge portfolio-nav-item"><a href="#contact-section"> <FontAwesomeIcon icon={faEnvelope}/> Contact</a></li>
-            <li className="hover-enlarge portfolio-nav-item"> <a href="#blog-section"> <FontAwesomeIcon icon={faComment}/> Blog</a></li>
-            <li className="hover-enlarge portfolio-nav-item"> <a href="#contributions-section"> <FontAwesomeIcon icon={faCodeCommit}/> Contributions</a></li>
+            <li className="hover-enlarge portfolio-nav-item"><a href="#about-section" class="portfolio-nav-link"> <FontAwesomeIcon icon={faCircleInfo}/> About</a> </li>
+            <li className="hover-enlarge portfolio-nav-item"><a href="#portfolio-section" class="portfolio-nav-link"> <FontAwesomeIcon icon={faCode}/> Portfolio </a></li>
+            <li className="hover-enlarge portfolio-nav-item"><a href="#contact-section" class="portfolio-nav-link"> <FontAwesomeIcon icon={faEnvelope}/> Contact</a></li>
+            <li className="hover-enlarge portfolio-nav-item"> <a href="#blog-section" class="portfolio-nav-link"> <FontAwesomeIcon icon={faComment}/> Blog</a></li>
+            <li className="hover-enlarge portfolio-nav-item"> <a href="#contributions-section" class="portfolio-nav-link"> <FontAwesomeIcon icon={faCodeCommit}/> Contributions</a></li>
             <HamburgerMenu/>
         </ul>
    </nav>
